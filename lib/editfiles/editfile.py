c875bf8069c0a617016cd90550e727afe89c823b
Richie Ngounou <r.ngounouyossa@technologyandstrategy.com>
2022-03-07 21:22:02 +0100

    edifile module usable function read and write with error management created.

--- lib/editfiles/editfile.py
+++ lib/editfiles/editfile.py
@@ -1,10 +1,20 @@
+"""
+ # @ Author: OmegaGae
+ # @ Create Time: 2022-03-03 13:41:10
+ # @ Modified by: OmegaGae
+ # @ Modified time: 2022-03-07 16:18:02
+ # @ Description: Ready for work !!
+ """
+
 #!/usr/bin/env python3
+from cmath import inf
 import os
+from typing import List
 
 
 ERRORS_EDITFILE = {
     0: "Data length is incorrect",
-    1: "This verb and his tenses is already known by the VerbenLernen App",
+    1: "This verb and his tenses are already known by the VerbenLernen App",
     2: "First element in the list is not a german infinitive verb",
     3: "The auxiliary enter is incorrect",
     4: "The level enter is incorrect",
@@ -35,20 +45,33 @@ class Editfile:
                 self.textfile = txt_file.readlines()
             return self.textfile
 
-    def _check_data_format(self, data_to_check: list):
+    @classmethod
+    def find_infinitive_verbs(cls, tense_verbs: list) -> List:
+
+        infinitive = []
+
+        for row_tense_verbs in tense_verbs:
+            infinitive.append(row_tense_verbs.split()[0])
+
+        return infinitive
+
+    @classmethod
+    def _check_data_format(cls, tense_verbs: list, data_to_check: list):
 
         if len(data_to_check) != 5:
             raise ValueError(ERRORS_EDITFILE.get(0))
 
-        if data_to_check[0] in self.readtxt():  # add for to find infinitive verbs
-            raise ValueError(ERRORS_EDITFILE.get(1))
+        len_infinitive_verb = len(data_to_check[0])
+        infinive = [
+            v for i, v in enumerate(data_to_check[0]) if i >= len_infinitive_verb - 2
+        ]
+        if (infinive == ["e", "n"]) is False:
+            if (infinive == ["r", "n"]) is False:
+                raise ValueError(ERRORS_EDITFILE.get(2))
 
-        len_infinitive = len(data_to_check[0])
-        if (
-            [v for i, v in enumerate(data_to_check[0]) if i >= len_infinitive - 2]
-            == ["e", "n"]
-        ) is False:
-            raise ValueError(ERRORS_EDITFILE.get(2))
+        infinive_verbs = Editfile.find_infinitive_verbs(tense_verbs)
+        if data_to_check[0] in infinive_verbs:
+            raise ValueError(ERRORS_EDITFILE.get(1))
 
         perfect = data_to_check[3].split()
         if (perfect[0] in ["hat", "ist"]) is False:
@@ -64,10 +87,12 @@ class Editfile:
 
         data_to_write: Data you want to write in the txt file. Language data must be german.
                        Waiting format: ["infinitive verb","conjugate verb to 3rd singular form","preterite verb in 3rd singular form","perfect with auxiliary in 3rd singular form","level:A1-C2"]
-                       example: ["fahren","Fährt", "Fuhr","ist gefahreeeeeeeeeen", "A2"]
+                       example: ["fahren","Fährt", "Fuhr","ist gefahren", "A2"]
         """
-        self._check_data_format(
-            data_to_write
+        txt = Editfile(self.filename)
+        lecture_txt = txt.readtxt()
+        Editfile._check_data_format(
+            lecture_txt, data_to_write
         )  # If nothing is raise, format is good, data can be written
         str_data_to_write = "\n" + " ".join(list(map(str, data_to_write)))
 
@@ -78,5 +103,4 @@ class Editfile:
 
 if __name__ == "__main__":
     input_vb = Editfile("starke_unregelmäßie.txt")
-    print(input_vb.readtxt())
-    input_vb.writetxt(["fahren", "Fährt", "Fuhr", "ist gefahren", "A2"])
+    input_vb.writetxt(["erinnern", "erinnert", "erinnerte", "hat erinnert", "A2"])
